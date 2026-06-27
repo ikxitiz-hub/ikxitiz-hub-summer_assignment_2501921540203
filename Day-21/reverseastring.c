cd1867367e12fab9dc54ab51e948ed8b0446691e
ikxitiz <kshitizag2904@gmail.com>
2026-06-27 23:28:19 +0530

    Day-21 done

---
+++ Day-21/reverseastring.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, len = 0;
+
+    gets(str);
+
+    while(str[len] != '\0')
+        len++;
+
+    for(i = len - 1; i >= 0; i--)
+        printf("%c", str[i]);
+
+    return 0;
+}
