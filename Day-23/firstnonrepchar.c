5b62183db768936916c692b586d88f51824b6f8d
ikxitiz <kshitizag2904@gmail.com>
2026-06-29 14:11:52 +0530

    Day-23 done

---
+++ Day-23/firstnonrepchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j, count;
+
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        count = 0;
+        for(j = 0; str[j] != '\0'; j++) {
+            if(str[i] == str[j])
+                count++;
+        }
+        if(count == 1) {
+            printf("First Non-Repeating Character = %c", str[i]);
+            return 0;
+        }
+    }
+
+    printf("No Non-Repeating Character");
+
+    return 0;
+}
---
+++ Day-23/maxoccuringchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    int i, max = 0;
+    char ch;
+
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++)
+        freq[(int)str[i]]++;
+
+    for(i = 0; i < 256; i++) {
+        if(freq[i] > max) {
+            max = freq[i];
+            ch = i;
+        }
+    }
+
+    printf("Maximum Occurring Character = %c", ch);
+
+    return 0;
+}
