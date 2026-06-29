9f77ceacefafbbef860ce04d429bdbff9b4ab707
ikxitiz <kshitizag2904@gmail.com>
2026-06-29 13:32:34 +0530

    Day-22 done

---
+++ Day-22/countwordsinsentence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, words = 1;
+
+    gets(str);
+
+    while(str[i] != '\0') {
+        if(str[i] == ' ')
+            words++;
+        i++;
+    }
+
+    printf("Words = %d", words);
+
+    return 0;
+}
---
+++ Day-22/removespaces.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, j = 0;
+
+    gets(str);
+
+    while(str[i] != '\0') {
+        if(str[i] != ' ') {
+            str[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+
+    str[j] = '\0';
+
+    printf("%s", str);
+
+    return 0;
+}
